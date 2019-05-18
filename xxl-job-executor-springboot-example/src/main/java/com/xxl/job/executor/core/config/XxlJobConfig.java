fb1d64a03ae43d7c05ed44a7a552c7009adee7d3
chengzhichao <chengzhichao@le.com>
2019-05-18 17:03:33 +0800

    凌乱的心情

--- xxl-job-executor-springboot-example/src/main/java/com/xxl/job/executor/core/config/XxlJobConfig.java
+++ xxl-job-executor-springboot-example/src/main/java/com/xxl/job/executor/core/config/XxlJobConfig.java
@@ -1,5 +1,9 @@
 package com.xxl.job.executor.core.config;
 
+import com.xxl.job.core.executor.XxlJobExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -8,6 +12,21 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:applicationcontext-xxl-job.xml")
 public class XxlJobConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
 
+    @Bean(initMethod = "start", destroyMethod = "destroy")
+    public XxlJobExecutor xxlJobExecutor() {
+        logger.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobExecutor xxlJobSpringExecutor = new XxlJobExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppName(appName);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+
+        return xxlJobSpringExecutor;
+    }
 
 }
