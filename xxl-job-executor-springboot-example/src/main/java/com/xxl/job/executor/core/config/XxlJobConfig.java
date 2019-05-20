57ee1fbb32ffccf04113cda828b346262c5423c1
chengzhichao <chengzhichao@le.com>
2019-05-20 11:42:11 +0800

    整理下代码， 官方给的SpringBoot的example感觉略乱

--- xxl-job-executor-springboot-example/src/main/java/com/xxl/job/executor/core/config/XxlJobConfig.java
+++ xxl-job-executor-springboot-example/src/main/java/com/xxl/job/executor/core/config/XxlJobConfig.java
@@ -3,28 +3,40 @@ package com.xxl.job.executor.core.config;
 import com.xxl.job.core.executor.XxlJobExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 
 @Configuration
-@ImportResource("classpath:applicationcontext-xxl-job.xml")
+// 这里我把这个注释掉了，因为我想读取我在application.properties中配置的属性
+//@ImportResource("classpath:applicationcontext-xxl-job.xml")
 public class XxlJobConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
 
+    @Value("${xxl.job.admin.addresses}")
+    private String addresses;
+    @Value("${xxl.job.executor.appname}")
+    private String appName;
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+    @Value("${xxl.job.executor.port}")
+    private int port;
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
     @Bean(initMethod = "start", destroyMethod = "destroy")
     public XxlJobExecutor xxlJobExecutor() {
         logger.info(">>>>>>>>>>> xxl-job config init.");
+
         XxlJobExecutor xxlJobSpringExecutor = new XxlJobExecutor();
-        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAdminAddresses(addresses);
         xxlJobSpringExecutor.setAppName(appName);
         xxlJobSpringExecutor.setIp(ip);
         xxlJobSpringExecutor.setPort(port);
-        xxlJobSpringExecutor.setAccessToken(accessToken);
         xxlJobSpringExecutor.setLogPath(logPath);
-        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
 
         return xxlJobSpringExecutor;
     }
